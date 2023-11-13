240544905b4aba4b71227cf9b162085de377690a
rashy <yagoubi.r@gmail.com>
2023-11-13 04:55:42 +0100

    Fix : relative path for public folder into absolute path

--- kriticart-server/src/index.ts
+++ kriticart-server/src/index.ts
@@ -1,21 +1,25 @@
 import express, { Request, Response, Application } from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import path from 'path'
 
 //For env File
 dotenv.config()
 
+//Instentiate server
 const app: Application = express()
 const port = process.env.PORT || 8000
 
+//Add middlewars
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+//Add routing
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript KriticArt server')
 })
-
-app.use(express.json())
-app.use(morgan('tiny'))
-app.use(express.static('public'))
-
+//Start Rest Api
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`)
 })
